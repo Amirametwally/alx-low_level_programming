3ae84cf86a109641e3763cced96264b844fdc37c
root <root@DESKTOP-C8FG64T.localdomain>
2023-07-19 22:23:39 +0300

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,19 +5,18 @@
  */
 void jack_bauer(void)
 {
-	int h, m;
+	int i, j;
 
-	for (h = 0; h < 24; h++)
+	for (i = 0; i < 24; i++)
 	{
-		for (m = 0; m < 60; m++)
+		for (j = 0; j < 60; j++)
 		{
-			_putchar((h / 10) + '0');
-			_putchar((h % 10) + '0');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
 			_putchar(':');
-			_putchar((m / 10) + '0');
-			_putchar((m % 10) + '0');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
 			_putchar('\n');
 		}
 	}
 }
-
