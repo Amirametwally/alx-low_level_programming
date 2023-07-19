cd609786e312c88ea784a84e7aac01b1365766a1
root <root@DESKTOP-C8FG64T.localdomain>
2023-07-19 05:42:52 +0300

    readme file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two numbers together.
+ * @i:  first number to be added.
+ * @j:  second number to be added.
+ * Return: sum of two numbers.
+ */
+int add(int i, int j)
+{
+	return (i + j);
+}
