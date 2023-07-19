2353454b9269f166ed6b6add322905c654061b05
root <root@DESKTOP-C8FG64T.localdomain>
2023-07-19 04:47:42 +0300

    readme file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -4,11 +4,11 @@
  *@c: The character to check  lowercase or uppercase
  * Return:  if c is a letter, lowercase or uppercase, 0 if it is otherwise.
  */
-int _isalpha(int c);
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
-
+		
 }
