19d8ca0e673ebb9ccf56572d222855fed3667270
root <root@DESKTOP-C8FG64T.localdomain>
2023-07-21 03:52:53 +0300

    done

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,5 +5,14 @@
 
 int _putchar(char c);
 int _isupper(int c);
+int _isdigit(int c);
+void print_triangle(int size);
+void print_square(int size);
+void print_diagonal(int n);
+void print_line(int n);
+void more_numbers(void);
+void print_most_numbers(void);
+void print_numbers(void);
+int mul(int a, int b);
 
 #endif
