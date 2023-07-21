71444855693069c6ce066cc3f978385b78a916be
root <root@DESKTOP-C8FG64T.localdomain>
2023-07-21 04:50:45 +0300

    done

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * more_numbers - prints 10 times the numbers, from 0 to 14
+ * more_numbers - nmbr fct
  *
  * Return: no return
  */
@@ -9,15 +9,15 @@
 void more_numbers(void)
 {
 
-	int i, j;
-	for (i = 0; i <= 9; i++)
+int i, j;
+	for (i = 1; i <= 10; i++)
 	{
-		for (j = 0; j < 14; j++)
+		for (j = 0; j < 15; j++)
 		{
-			if (j > 9)
-				_putchar(j / 10 + '0');
+			if (j >= 10)
+			_putchar('1');
 			_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
-	}
+			_putchar('\n');
+		}
 }
