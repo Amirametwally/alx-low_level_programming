5f4991df719a149590eb9adb259f0b1e17966f11
root <root@DESKTOP-C8FG64T.localdomain>
2023-07-22 00:42:11 +0300

    done

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,25 +8,16 @@
  */
 void print_number(int i)
 {
-	if (i / 10 != 0)
-	{
-		print_number(i / 10);
-		if (i > 0)
-		{
-			_putchar(i % 10 + '0');
-		}
-		else
-		{
-			_putchar(-i % 10 + '0');
-		}
-	}
-	else if ((i / 10 == 0) && (i % 10 != 0) && (i > 0))
-	{
-		_putchar(i % 10 + '0');
-	}
-	else if ((i / 10 == 0) && (i % 10 != 0) && (i <= 0))
+	if (i < 0)
 	{
 		_putchar('-');
-		_putchar(-i % 10 + '0');
+		i = -i;
 	}
+
+	if (i / 10 > 0)
+	{
+		print_number(i / 10);
+	}
+
+	_putchar(i % 10 + '0');
 }
