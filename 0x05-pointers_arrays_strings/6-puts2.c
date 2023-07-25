0347ccad440fcfd9a51c9b63295d3c99faedda28
root <root@DESKTOP-C8FG64T.localdomain>
2023-07-25 04:06:24 +0300

    done

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -2,7 +2,7 @@
 
 /**
  * puts2 - Prints every other character of a string starting with the first
- *         character, followed by a new line
+ * character, followed by a new line
  * @str:  string to print
  *
  * Return: nothing
@@ -10,8 +10,14 @@
 void puts2(char *str)
 {
 	int i;
+	int len = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	for (i = 0; i < len ; i += 2)
 	{
 		_putchar(str[i]);
 	}
