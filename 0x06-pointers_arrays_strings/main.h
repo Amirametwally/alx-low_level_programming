652a78206be39e66150f3caaab1462dda61e7deb
root <root@DESKTOP-C8FG64T.localdomain>
2023-07-28 04:45:20 +0300

    done

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,6 +9,11 @@ int _strcmp(char *s1, char *s2);
 char *cap_string(char *);
 char *string_toupper(char *);
 void reverse_array(int *a, int n);
+char *leet(char *);
 char *rot13(char *);
+void print_buffer(char *b, int size);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_number(int n);
+
 
 #endif
