ddaec973bed89d8fce5fb5ca61aeb3f8d6d213f4
root <root@DESKTOP-C8FG64T.localdomain>
2023-07-28 04:58:16 +0300

    done

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number -  function that prints an integer
+ * @n: integr that printed
+ *
+ * Return: nothing
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+
+	if (n / 10 > 0)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar(n % 10 + '0');
+
+}
