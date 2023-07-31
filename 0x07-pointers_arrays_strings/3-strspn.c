3d824dabd1adadda9828a954bf0462aa17f8a9f8
root <root@DESKTOP-C8FG64T.localdomain>
2023-07-31 22:52:02 +0300

    done

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - function  calculates  the  length (in bytes) of the initial
+ * segment of s which consists entirely of bytes in accept.
+ * @s: string
+ * @accept: bytes
+ *
+ * Return: length
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				break;
+			}
+		}
+		if (accept[j] == '\0')
+		{
+			return i;
+		}
+	}
+	return (i);
+}
