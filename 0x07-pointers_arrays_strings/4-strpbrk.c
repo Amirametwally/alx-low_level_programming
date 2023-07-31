b8304a4c8b5a863c6fabd628ddd818b600facc67
root <root@DESKTOP-C8FG64T.localdomain>
2023-08-01 00:05:58 +0300

    done

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ */
+char *_strpbrk(char *s, char *accept)
+{
+ int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (s[i] != '\0')
+	{
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+				return ((s + i));
+			j++;
+		}
+		i++;
+	}
+	return (NULL);
+}
