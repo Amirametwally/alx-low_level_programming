a80c84111aa1a87461fec001f40326377f131842
root <root@DESKTOP-C8FG64T.localdomain>
2023-07-31 23:03:15 +0300

    done

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,6 +12,7 @@
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int i, j;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
@@ -23,7 +24,7 @@ unsigned int _strspn(char *s, char *accept)
 		}
 		if (accept[j] == '\0')
 		{
-			return i;
+			return (i);
 		}
 	}
 	return (i);
