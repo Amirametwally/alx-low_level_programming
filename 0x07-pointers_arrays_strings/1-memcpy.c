ad305f47bac8a2add7260b1883002255d9a28dfa
root <root@DESKTOP-C8FG64T.localdomain>
2023-07-31 19:35:55 +0300

    done

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - function that copies memory area
+ * @dest:destination
+ * @src: the source
+ * @n: bytes of the memory area
+ *
+ * Return: dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
