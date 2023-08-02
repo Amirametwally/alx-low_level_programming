91cf4461999e6c8c844aadba53a0de0fe6fdf4dc
root <root@DESKTOP-C8FG64T.localdomain>
2023-08-02 21:15:15 +0300

    done

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - function that returns the length of a string.
+ * @s: string
+ *
+ * Return: Length
+ */
+int _strlen_recursion(char *s)
+{
+	int i = 0;
+
+	if (s[i] == '\0')
+	{
+		return (0);
+	}
+
+	i++;
+	return (1 + _strlen_recursion(s + i));
+}
