4c5adee36f08d0ddb985530333754a08a11534f7
Amirametwally <amira.metwally56@gmail.com>
2023-08-09 23:26:46 +0300

    done

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,7 +1,42 @@
-#include "bootcamp.h"
+#include "main.h"
 /**
  * argstostr - concatenates all the arguments of program
  * @ac: count of args
  * @av: pointer to array of args
  * Return: pointer to new array of all args or NULL if failure
  */
+char *argstostr(int ac, char **av)
+{
+if (ac == 0 || av == NULL)
+	return (NULL);
+
+int total_length = 0;
+int i, j;
+char *str;
+
+for (i = 0; i < ac; i++)
+	{
+	total_length += strlen(av[i]) + 1;
+}
+
+str = malloc(sizeof(char) * total_length + 1);
+
+if (str == NULL)
+	return (NULL);
+
+int position = 0;
+for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+		str[position] = av[i][j];
+			position++;
+		}
+		str[position] = '\n';
+		position++;
+}
+
+str[position] = '\0';
+
+return (str);
+}
