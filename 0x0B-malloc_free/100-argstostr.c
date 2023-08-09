9cdf184c537439c7580e8c8c3e89dec858a47859
Amirametwally <amira.metwally56@gmail.com>
2023-08-09 23:48:45 +0300

    done

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -7,36 +7,33 @@
  */
 char *argstostr(int ac, char **av)
 {
-if (ac == 0 || av == NULL)
-	return (NULL);
+	char *str;
+	int length, i, j, d;
 
-int total_length = 0;
-int i, j;
-char *str;
-
-for (i = 0; i < ac; i++)
+	if (ac == 0)
+		return (NULL);
+	if (av == 0)
+		return (NULL);
+	length = 0;
+	for (i = 0; i < ac; i++)
 	{
-	total_length += strlen(av[i]) + 1;
-}
-
-str = malloc(sizeof(char) * total_length + 1);
-
-if (str == NULL)
-	return (NULL);
-
-int position = 0;
-for (i = 0; i < ac; i++)
+		for (j = 0; av[i][j] != '\0'; j++)
+			length++;
+		length++;
+	}
+	length++;
+	str = malloc(length * sizeof(char));
+	if (str == 0)
+		return (NULL);
+	d = 0;
+	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
-		str[position] = av[i][j];
-			position++;
+			str[d++] = av[i][j];
 		}
-		str[position] = '\n';
-		position++;
-}
-
-str[position] = '\0';
-
-return (str);
+		str[d++] = '\n';
+	}
+	str[d] = '\0';
+	return (str);
 }
