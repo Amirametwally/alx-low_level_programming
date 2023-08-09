0b66f020ab045d9a19368481242a72434e42c43e
Amirametwally <amira.metwally56@gmail.com>
2023-08-09 23:11:31 +0300

    done

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,7 @@
+#include "bootcamp.h"
+/**
+ * argstostr - concatenates all the arguments of program
+ * @ac: count of args
+ * @av: pointer to array of args
+ * Return: pointer to new array of all args or NULL if failure
+ */
