9ebda89d59e86df581cbc5239fa5a8c0616acf7e
Amirametwally <amira.metwally56@gmail.com>
2023-08-09 13:25:10 +0300

    done

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,6 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
+
 /**
  * _strdup - copies input string in newly allocated memory
  * @str: input string
@@ -8,17 +7,26 @@
  */
 char *_strdup(char *str)
 {
-	char *pstr;
-	unsigned int i = 0;
+	char *ptr;
+	int i = 0;
 
 	if (str == 0)
 		return (0);
+
+	if (ptr == NULL)
+		return (NULL);
+
 	while (str[i] != 0)
 		i++;
+
 	i++;
-	pstr = malloc(sizeof(char) * i);
+
+	ptr = malloc(sizeof(char) * i);
+
 	for (i = 0; str[i] != 0; i++)
-		pstr[i] = str[i];
-	pstr[i] = 0;
-	return (pstr);
+		ptr[i] = str[i];
+
+	ptr[i] = '\0';
+
+	return (ptr);
 }
