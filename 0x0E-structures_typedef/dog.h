aca9b45463fe8e3c690dfbde7881612f86701613
Amirametwally <amira.metwally56@gmail.com>
2023-08-14 10:49:13 +0300

    done

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,24 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - information about dog
+ * @name: dog's name
+ * @age: age of the dog
+ * @owner: name of the owner of the dog
+ *
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+
+
+
+#endif
