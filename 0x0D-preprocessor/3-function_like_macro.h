5b2003c30be1abde6299be62b6293bb1aae56516
Amirametwally <amira.metwally56@gmail.com>
2023-08-15 02:14:46 +0300

    done

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,7 +1,6 @@
-#ifndef FUNCTION_LIKE_MACROH_H
-#define FUNCTION_LIKE_MACROH_H
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) (((x) < (0))? (-x) : (x))
+#define ABS(x) ((x) <  0 ? (-x) : (x))
 
 #endif
-
