10961ac340a6bf513945ea49560ebd5feb2bd45f
Amirametwally <amira.metwally56@gmail.com>
2023-08-16 20:25:14 +0300

    done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name.
+ * @name: string 
+ * @f: fuction 
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+if (!f || !name)
+{
+return;    
+}
+else
+{
+(*f)(name);    
+}
+}
