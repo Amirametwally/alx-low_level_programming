99e007b4cbd0a50c0f7187318d3ddf556734a274
Amirametwally <amira.metwally56@gmail.com>
2023-08-18 01:00:57 +0300

    done

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -10,6 +10,13 @@ int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+typedef struct print
+{
+char *param;
+void (*f)(va_list);
+} print_t;
+
 void print_all(const char * const format, ...);
 
 
