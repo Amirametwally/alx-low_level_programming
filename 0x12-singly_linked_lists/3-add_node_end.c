ebe1d005986fdaccd1f48b41304a4dc0b1d87a09
Amirametwally <amira.metwally56@gmail.com>
2023-08-25 01:12:35 +0300

    done

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -20,7 +20,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
-	  return (NULL);
+	{
+		return (NULL);
+	}
 	new->len = i;
 	new->str = strdup(str);
 	new->next = NULL;
