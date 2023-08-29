41bb32f71dadc6948d8db6b01bf04509816f974b
Amirametwally <amira.metwally56@gmail.com>
2023-08-29 19:01:06 +0300

    done

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -23,6 +23,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	newNode->n = n;
 
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+
 	ptr = *head;
 	for (i = 0; i < idx - 1; i++)
 	{
