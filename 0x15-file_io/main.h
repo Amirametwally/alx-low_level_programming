d7418fa3fd0391adbbe0d244b573d8c7b06c0836
Amirametwally <amira.metwally56@gmail.com>
2023-09-07 19:49:55 +0300

    done

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,10 +5,24 @@
 #include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+
+/**
+ * print_abi - Prints the ABI version of an ELF header.
+ * @e_ident: A pointer to an array containing the ELF ABI version.
+ */
+void print_abi(unsigned char *e_ident)
+{
+	printf("  ABI Version:                       %ld\n",
+				 e_ident[EI_ABIVERSION]);
+}
 
 #endif
