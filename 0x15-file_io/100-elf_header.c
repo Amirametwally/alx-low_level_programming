48ae72afe16e169d88ac54db0043209713e1dcb6
Amirametwally <amira.metwally56@gmail.com>
2023-09-07 21:27:59 +0300

    done

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -123,7 +123,7 @@ void close_elf(int elf)
 	if (close(elf) == -1)
 	{
 		dprintf(STDERR_FILENO,
-						"Error: Can't close fd %d\n", elf);
+						"Error: Can't close fd %u\n", elf);
 		exit(98);
 	}
 }
