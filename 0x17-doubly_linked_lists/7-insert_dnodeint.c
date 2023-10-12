fc52d7819e5738554fafb213e4d5fd5310675954
Amirametwally <amira.metwally56@gmail.com>
2023-10-13 01:37:23 +0300

    done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -45,10 +45,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	}
 	else
 	{
-		new_node->next = ptr->next;
-		new_node->prev = ptr;
-		ptr->next->prev = new_node;
-		ptr->next = new_node;
+		new_node->next = ptr;
+		new_node->prev = ptr->prev;
+		if (ptr->prev != NULL)
+			ptr->prev->next = new_node;
+		ptr->prev = new_node;
 	}
 	return (new_node);
 }
