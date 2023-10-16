5c7dad69ec05e0f1cb984286612624d03a74a88e
Amirametwally <amira.metwally56@gmail.com>
2023-10-16 22:43:13 +0300

    done

---
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strchr - function that locates a character in a string
+ * @s: the string
+ * @c: the character
+ * Return: pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+			return ((s + i));
+		i++;
+	}
+	if (c == '\0')
+		return ((s + i));
+	return (NULL);
+}
